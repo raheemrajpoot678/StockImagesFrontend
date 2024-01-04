5887bc9e76668ff3264162c36b68807a83063d51
raheemrajpoot678 <raheemrajpoot678@gmail.com>
2024-01-04 13:42:11 +0500

    update

--- src/data.js
+++ src/data.js
@@ -251,7 +251,7 @@ export const heroImg = [
   {
     id: 7,
     imgSrc:
-      "https://images.unsplash.com/photo-1528127110434-c2fd7f12cd6e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1703328742708-d547eed7a0f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 8,
